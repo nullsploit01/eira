97e18b2c669564422fc5f55876167d3f42fcba54
nullsploit01 <harshaldharmik099@gmail.com>
2026-05-22 18:38:06 +0530

    remove extra file

--- v1/constants/company/index.ts
+++
@@ -1,8 +0,0 @@
-const company = {
-  NAME: 'Bushel Technologies Private Limited',
-  SHORT_NAME: 'Bushel',
-  LINKEDIN: 'https://www.linkedin.com/company/bushel-technologies-private-limited/',
-  JOBTITLE: 'Software Engineer'
-};
-
-export default company;
